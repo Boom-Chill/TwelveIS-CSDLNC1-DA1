9029adef6dd72ddbd7c109f759cfc01d364597a0
tranvinhphat0202 <19127057@student.hcmus.edu.vn>
2021-11-11 16:14:36 +0700

    create react da1 app, re-use components, summary page

---
+++ 3-Phat (create react app, re-use components, summary page)/da1/src/pages/Invoice/Invoice.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Invoice.scss'
+
+function Invoice(props) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Invoice;
---
+++ 3-Phat (create react app, re-use components, summary page)/da1/src/pages/UploadInvoice/UploadInvoice.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UploadInvoice.scss'
+
+function UploadInvoice(props) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default UploadInvoice;
